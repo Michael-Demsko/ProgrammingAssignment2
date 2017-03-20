1b14862c009f1a3c0d671be01c9c301da281fede
Michael-Demsko <dmicha4@vt.edu>
2017-03-20 00:37:04 -0400

    Revert "Programming Assignment 2 Resubmission"
    This reverts commit 023a09fa02af9e0e6dffffb5ad91357efa58d50e.

--- Assignment2Function.R
+++ Assignment2Function.R
@@ -1,33 +1,26 @@
 makeCacheMatrix <- function(x = matrix()) {
-## This funciton wil cache the inverse of its matrix argument
-  inv <- NULL ## Initialize inverse matrix as NULL
+  m <- NULL
   set <- function(y) {
-  ## Assign new value to matricies in parent environment
     x <<- y
-    inv <<- NULL
+    m <<- NULL
   }
-  get <- function() x ## Returns value of matrix argument
-  setinverse <- function(inverse) inv <<- inverse
-  ## Computes inverse of matrix argument and assigns computed value to parent environment
-  getinverse <- function() inv
-  ## Gets value of inverse matrix
+  get <- function() x
+  setinverse <- function(inverse) m <<- solve
+  getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
-  ## Necessary in order to refer to functions with $ operator
+  
 }
 
 cacheSolve <- function(x, ...) {
-  ## This function will check if the inverse matrix has been computed an cached
-  ## If so, cached matrix is retrieved. If not, value is calculated and assigned
-  ## via setinverse()
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    return(m)
   }
   data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
-  inv
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
